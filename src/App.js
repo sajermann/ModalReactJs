56ba4eee500b310b1dd5bc48e845a912c263d3ff
Bruno Sajermann <contato@sajermann.com>
2020-06-09 15:13:19 -0300

    Modal Complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Modal from './Components/Modal';
 
 function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() =>{setIsModalVisible(true)}}>Open</button>
+      {
+        isModalVisible && 
+        <Modal onClose={()=>{setIsModalVisible(false)}} >
+          <h2>Modal do App</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum consequuntur non iure suscipit nostrum earum inventore sequi nihil vero incidunt enim, architecto ratione dolorem consequatur eligendi, voluptatum similique saepe quidem?</p>
+        </Modal>
+      }
     </div>
   );
 }
